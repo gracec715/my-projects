b8ecff617030185270eb471c1a3e1bf3ef4e2f11
grace chang <grace715@graces-MacBook-Air.local>
2025-05-24 17:02:09 -0700

    insert extra spaces

--- dic.py
+++ dic.py
@@ -9,7 +9,7 @@ if 'alice' not in favorite_language.keys():
     print("Alice, please take our poll!")
 
 for name in sorted(favorite_language.keys()):
-    print(f"{name.title()}, thank you for taking the poll.")
+    print(f"\n{name.title()}, thank you for taking the poll.")
 
 print("\nThe following languages have been mentioned:")
 for language in favorite_language.values():
